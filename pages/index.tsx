0c52f1f5c9e5d33e8d6e71e58352d07f11253764
flamingotiger <hhbhong@gmail.com>
2020-02-19 16:38:40 +0900

    Add typescript setting

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+import Nav from '../src/components/nav'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Nav />
+  </div>
+)
+
+export default Home
