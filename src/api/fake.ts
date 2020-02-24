4a992555ae9bd7961ffe47366b268dd6ad72dbc6
flamingotiger <hhbhong@gmail.com>
2020-02-24 13:40:35 +0900

    Fix fake api

--- src/api/fake.ts
+++ src/api/fake.ts
@@ -11,14 +11,14 @@ export const FakeGetItemApi = (id, params): Promise<FakePostItem> => {
 	return axios.get(endpoint + id, { params });
 };
 
-export const FakeDeleteItemApi = (params): Promise<FakePostItem> => {
-	return axios.delete(endpoint, { params });
+export const FakeDeleteItemApi = (id): Promise<FakePostItem> => {
+	return axios.delete(endpoint + id);
 };
 
-export const FakeUpdateItemApi = (params, body): Promise<FakePostItem> => {
-	return axios.put(endpoint, body, { params });
+export const FakeUpdateItemApi = (id, params, body): Promise<FakePostItem> => {
+	return axios.put(endpoint + id, body, { params });
 };
 
-export const FakeAddItemApi = (id, body): Promise<FakePostItem> => {
-	return axios.post(endpoint + id, body);
+export const FakeAddItemApi = (body): Promise<FakePostItem> => {
+	return axios.post(endpoint, body);
 };
