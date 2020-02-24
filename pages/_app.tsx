6ca2bcb42dcc341dafa8a2aafe0aa78297522a8c
flamingotiger <hhbhong@gmail.com>
2020-02-24 17:59:04 +0900

    Add redux wrapper and logo image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import App from 'next/app';
 import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import withRedux from 'next-redux-wrapper';
 import GlobalStyles from '../src/common/styles/global-styles';
 import store from '../src/store';
 
@@ -21,4 +22,4 @@ class ReactApp extends App<any> {
 	}
 }
 
-export default ReactApp;
+export default withRedux(() => store, { debug: process.env.NODE_ENV === 'development' })(ReactApp);
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 
 const Button = styled.button`
 	background-color: #fdd835;
-	height: 40px;
-	width: 160px;
+	height: 50px;
+	width: 180px;
 	text-align: center;
 	line-height: 30px;
 	font-size: 20px;
@@ -29,27 +29,36 @@ const Button = styled.button`
 
 const Ul = styled.ul`
 	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-top: 20px;
+	margin-bottom: 20px;
 `;
 
-const Nav = () => (
-	<nav>
-		<Ul>
-			<li>
-				<Button>
-					<Link href="/">
-						<a href="/">List</a>
-					</Link>
-				</Button>
-			</li>
-			<li>
-				<Button>
-					<Link href="/form">
-						<a href="/form">Form</a>
-					</Link>
-				</Button>
-			</li>
-		</Ul>
-	</nav>
-);
+const Nav = () => {
+	return (
+		<nav style={{ width: '800px', margin: 'auto' }}>
+			<div>
+				<img src="/background_logo.jpg" alt="nextjs custom starter" />
+			</div>
+			<Ul>
+				<li>
+					<Button>
+						<Link href="/">
+							<a href="/">List</a>
+						</Link>
+					</Button>
+				</li>
+				<li>
+					<Button>
+						<Link href="/form">
+							<a href="/form">Form</a>
+						</Link>
+					</Button>
+				</li>
+			</Ul>
+		</nav>
+	);
+};
 
 export default Nav;
