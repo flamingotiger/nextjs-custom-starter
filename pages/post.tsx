6ced4f2348017ec4c6a6a4fe1fa1bc3dda6f525e
hakbum <hhbhong@gmail.com>
2020-03-06 15:07:36 +0900

    Add post list

--- pages/post.tsx
+++ pages/post.tsx
@@ -1,13 +1,27 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { FakeActionType } from '../src/store/reducers/fake';
+import { FakePostItem } from '../src/api/types';
+import { RootState } from 'src/store/reducers';
 
 const Post: React.FC = () => {
+	const fakes = useSelector((state: RootState) => state.fake.list);
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch({ type: FakeActionType.GET_FAKE_LIST_REQUEST });
 	}, []);
-	return <div>test</div>;
+	return (
+		<div>
+			{fakes.map((fake: FakePostItem) => (
+				<div key={fake.id}>
+					<h3 style={{ fontSize: '20px', fontWeight: 'bold', textTransform: 'uppercase', marginBottom: '20px' }}>
+						{fake.title}
+					</h3>
+					<p style={{ fontSize: '14px' }}>{fake.body}</p>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Post;
