0bc4f667237b4ad0d742adbd6c7f4c9b56a7c60c
hakbum <hhbhong@gmail.com>
2020-03-31 17:31:30 +0900

    Add images optimized

--- next.config.js
+++ next.config.js
@@ -1,15 +1,29 @@
-const withCSS = require('@zeit/next-css');
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const withOptimizedImages = require('next-optimized-images');
 
 require('dotenv').config();
 
-module.exports = withCSS({
+module.exports = withOptimizedImages({
 	typescript: {
 		ignoreDevErrors: true
 	},
+	distDir: './dist',
+	imagesName: '[hash].[ext]',
 	target: 'serverless',
 	env: {
 		BACKEND_URL: process.env.BACKEND_URL,
 		FAKE_URL: process.env.FAKE_URL,
 		PORT: process.env.PORT
+	},
+	webpack(config) {
+		// eslint-disable-next-line no-param-reassign
+		config.resolve.plugins = [
+			new TsconfigPathsPlugin({
+				configFile: path.resolve(__dirname, './tsconfig.json')
+			})
+		];
+		return config;
 	}
 });
+
