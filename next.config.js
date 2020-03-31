a60363c66857d38d0178c7e91d9b869a012b8d01
hakbum <hhbhong@gmail.com>
2020-03-31 14:12:38 +0900

    Fix config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ module.exports = withCSS({
 		ignoreDevErrors: true
 	},
 	target: 'serverless',
-	distDir: '.build',
 	env: {
 		BACKEND_URL: process.env.BACKEND_URL,
 		FAKE_URL: process.env.FAKE_URL,
