c72b19468d9e37f04db81474d5f7a3bb8e53aa78
hakbum <hhbhong@gmail.com>
2020-04-01 15:09:27 +0900

    Fix locales

--- src/__mocks__/mockReactI18next.ts
+++ src/__mocks__/mockReactI18next.ts
@@ -1,24 +1,22 @@
-let selectedLanguage = "en";
+let selectedLanguage = 'en';
 const useMock = {
-  t: (k: string) => k,
-  i18n: {
-    language: selectedLanguage,
-    changeLanguage: (lang: string) => (selectedLanguage = lang)
-  }
+	t: (k: string) => k,
+	i18n: {
+		language: selectedLanguage,
+		changeLanguage: (lang: string) => (selectedLanguage = lang)
+	}
 };
 
-const reactI18next = jest.mock("react-i18next", () => ({
-  withTranslation: jest
-    .fn()
-    .mockImplementation(() => (Component: React.FC | React.ComponentClass) => {
-      const ComponentCopy = { ...Component };
-      ComponentCopy.defaultProps = {
-        ...Component.defaultProps,
-        t: (k: string) => k
-      };
-      return ComponentCopy;
-    }),
-  useTranslation: () => useMock
+const reactI18next = jest.mock('react-i18next', () => ({
+	withTranslation: jest.fn().mockImplementation(() => (Component: React.FC | React.ComponentClass) => {
+		const ComponentCopy = { ...Component };
+		ComponentCopy.defaultProps = {
+			...Component.defaultProps,
+			t: (k: string) => k
+		};
+		return ComponentCopy;
+	}),
+	useTranslation: () => useMock
 }));
 
 export default reactI18next;
